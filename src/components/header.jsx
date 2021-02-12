ef3fdd79727d8ceffc2f9997690f2fbad8f59388
Сергей Грищенков <grishenkov.sergei@gmail.com>
2021-02-12 22:44:40 +0300

    no css

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,7 +2,35 @@ import React from "react";
 
 const Header = () => {
     return (
-        true
+        <header className="header">
+            <div className="header__inner">
+                <div className="header__logo-container">
+                    <img src="./img/logo-vector.svg" alt="Логотип компании" className="header__img" />
+                    <p className="header__logo-text">ЛИГА Банк</p>
+                </div>
+                <ul className="header__nav">
+                    <li className="header__nav-item">
+                        <a href="#" className="header__link">Услуги</a>
+                    </li>
+                    <li className="header__nav-item">
+                        <a href="#" className="header__link">Рассчитать кредит</a>
+                    </li>
+                    <li className="header__nav-item header__nav-item--active">
+                        <a href="#" className="header__link">Конвертер валют</a>
+                    </li>
+                    <li className="header__nav-item">
+                        <a href="#" className="header__link">Контакты</a>
+                    </li>
+                    <li className="header__nav-item">
+                        <a href="#" className="header__link">Задать вопрос</a>
+                    </li>
+                    <li className="header__nav-item header__nav-item--login">
+                        <a href="#" className="header__link">Войти в Интернет-банк</a>
+                    </li>
+                </ul>
+
+            </div>
+        </header>
     );
 };
 
