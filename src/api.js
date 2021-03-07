232ba911b6419237fd0b955c3794206ffc4907cd
Сергей Грищенков <grishenkov.sergei@gmail.com>
2021-03-07 08:57:26 +0300

    final

--- src/api.js
+++ src/api.js
@@ -4,13 +4,6 @@ export const getExchangeRate = (fromCurrency, toCurrency, date) => {
   const from = `${fromCurrency}_${toCurrency}`;
   const to = `${toCurrency}_${fromCurrency}`;
 
-  // return new Promise((resolve) => {
-  //     setTimeout(() => {
-  //       resolve({
-  //         [`${fromCurrency}_${toCurrency}`]: 47.935998,
-  //         [`${toCurrency}_${fromCurrency}`]: 0.020859,
-  //       });
-  //     }, 1000);
   return fetch(
       `https://free.currconv.com/api/v7/convert?q=${from},${to}&date=${date}&compact=ultra&apiKey=${API_KEY}`
   ).then((response) => {
