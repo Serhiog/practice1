75ea8fda1d1e3e88dd6909025832d7b7eaf41f4e
Sergey <54043966+Serhiog@users.noreply.github.com>
2021-03-21 06:56:35 -0500

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <header className="header">
       <div className="header__inner">
         <div className="header__logo-container">
-          <img src="../img/logo.svg" alt="Лига БАНК" className="header__img" width={28}
+          <img src="./img/logo.svg" alt="Лига БАНК" className="header__img" width={28}
             height={25} />
           <p className="header__logo-text">ЛИГА Банк</p>
         </div>
